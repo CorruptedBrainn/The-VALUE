e452535b4e1c05f4383996ad89f8d877caf09e29
Nicolas Martens <nicolas@martens.co.nz>
2025-05-15 01:33:25 +1200

    Generic Commit

--- Source/main.py
+++ Source/main.py
@@ -1,2 +1,22 @@
-import pygame
+from asyncio.windows_events import NULL
+import sys
 
+import main_menu as mainMenu
+import main_game as mainGame
+import main_editor as mainEditor
+
+from PySide6 import QtWidgets as QtW
+
+application = NULL
+
+def openApplication():
+    app = QtW.QApplication(sys.argv)
+    label = QtW.QLabel("Hello World!")
+    label.show()
+    app.exec()
+
+def main():
+    openApplication()
+
+if __name__ == "__main__":
+    main()

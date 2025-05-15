d9623ff12777d6c8fb23ce60c52b58d4fd78a315
Nicolas Martens <nicolas@martens.co.nz>
2025-05-16 02:13:49 +1200

    Committing to merge

---
+++ Source/main.py
@@ -0,0 +1,32 @@
+import sys
+
+import main_menu as mainMenu
+import main_game as mainGame
+import main_editor as mainEditor
+
+from PySide6 import QtWidgets as QtW
+from PySide6 import QtCore as QtC
+
+class window(QtW.QMainWindow):
+    def __init__(self, parent = None):
+        super().__init__(parent)
+        
+        # Central Widget
+        self.grapics = QtW.QGraphicsScene()
+        self.grapics.addText("The VALUE")
+        self.startButton = QtW.QPushButton()
+
+        self.view = QtW.QGraphicsView(self.grapics)
+        self.setCentralWidget(self.view)
+
+def openApplication():
+    app = QtW.QApplication(sys.argv)
+    mainWindow = window()
+    mainWindow.show()
+    return app.exec()
+
+def main():
+    sys.exit(openApplication())
+
+if __name__ == "__main__":
+    main()
---
+++ Source/main_editor.py
@@ -0,0 +1,2 @@
+from PySide6 import QtWidgets as QtW
+from PySide6 import QtCore as QtC

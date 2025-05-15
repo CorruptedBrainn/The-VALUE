4e4318495f233b32d8dbb260d1d8d315bf2af142
Nicolas Martens <nicolas@martens.co.nz>
2025-05-16 00:23:38 +1200

    Update on Menu

--- Source/main.py
+++ Source/main.py
@@ -5,9 +5,24 @@ import main_game as mainGame
 import main_editor as mainEditor
 
 from PySide6 import QtWidgets as QtW
+from PySide6 import QtCore as QtC
+
+class window(QtW.QMainWindow):
+    def __init__(self, parent: QtW.QMainWindow = None):
+        super().__init__(parent)
+        
+        # Central Widget
+        self.grapics = QtW.QGraphicsScene()
+        self.grapics.addText("The VALUE")
+        self.startButton = QtW.QPushButton()
+
+        self.view = QtW.QGraphicsView(self.grapics)
+        self.setCentralWidget(self.view)
 
 def openApplication():
     app = QtW.QApplication(sys.argv)
+    mainWindow = window()
+    mainWindow.show()
     return app.exec()
 
 def main():

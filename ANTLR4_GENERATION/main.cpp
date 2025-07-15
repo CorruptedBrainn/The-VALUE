e8b2b35934b649e082c85b690c2be2df3707c9d1
Nicolas Martens <nicolas@martens.co.nz>
2025-07-16 10:58:35 +1200

    COMMIT

--- ANTLR4_GENERATION/main.cpp
+++ ANTLR4_GENERATION/main.cpp
@@ -114,30 +114,3 @@ extern "C" {
 		return obj->executePrograms();
 	}
 }
-
-/*
-TEST:
-
-@typenames: data
-segment_tree {
-	size->int = 0;
-	tree->array<data> = {};
-
-	update(pos->int, val->data, i->int = 0, l->int = 0, r->int = size - 1)->data {
-		if (l == r) return tree[i] += val;
-		m->int = (l + r ) / 2;
-		if (pos <= m) return tree[i] = update(pos, val, i + 1, l, m) + tree[i + 2 * (m - l + 1)];
-		else return tree[i] = tree[i + 1] + update(pos, val, i + 2 * (m - l + 1), m + 1, r);
-	}
-
-	query(posl->int, posr->int, i->int = 0, l->int = 0, r->int = size - 1)->data {
-		if (posl <= l && r <= posr) return tree[i];
-		if (r < posl || posr < l) return NULL;
-		m->int = (l + r) / 2;
-		return query(posl, posr, i + 1, l, m) + query(posl, posr, i + 2 * (m - l + 1), m + 1, r);
-	}
-}
-
-static segtree->segment_tree<int>;
-print(segtree.query(2, 4));
-*/
